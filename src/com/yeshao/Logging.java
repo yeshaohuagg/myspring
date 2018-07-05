5a5ca062ade425c3272578988f5fd364e4498e0c
yeshaohua <827109043@qq.com>
2018-07-05 16:04:10 +0800

    AOP的java代码使用

--- src/com/yeshao/Logging.java
+++ src/com/yeshao/Logging.java
@@ -1,15 +1,24 @@
 package com.yeshao;
 
+import org.aspectj.lang.annotation.*;
+
+@Aspect
 public class Logging {
+    @Pointcut("execution(* com.yeshao.*.*(..))")
+    private void pointcut(){}
+    @Before("pointcut()")
     public void beforeAdvice(){
         System.out.println("Going to setup student profile");
     }
+    @After("pointcut()")
     public void afterAdvice(){
         System.out.println("Student profile has been setup");
     }
+    @AfterReturning(pointcut = "pointcut()",returning = "retVal")
     public void afterReturningAdvice(Object retVal){
         System.out.println("Returning:"+retVal.toString());
     }
+    @AfterThrowing(pointcut = "pointcut()",throwing = "e")
     public void afterThrowingAdvice(IllegalArgumentException e){
         System.out.println("There has been an exception:"+e.toString());
     }
