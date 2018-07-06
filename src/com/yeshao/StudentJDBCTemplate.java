4cf1fb0bc68885b10629f9dbc78920567aa5b6e7
yeshaohua <827109043@qq.com>
2018-07-06 14:44:43 +0800

    JDBC事物xml配置

--- src/com/yeshao/StudentJDBCTemplate.java
+++ src/com/yeshao/StudentJDBCTemplate.java
@@ -13,15 +13,15 @@ import java.util.List;
 public class StudentJDBCTemplate {
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
-    private PlatformTransactionManager transactionManager;
+//    private PlatformTransactionManager transactionManager;
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void create(String name, Integer age, Integer marks, Integer year) {
-        TransactionDefinition definition = new DefaultTransactionDefinition();
-        TransactionStatus status = transactionManager.getTransaction(definition);
+//        TransactionDefinition definition = new DefaultTransactionDefinition();
+//        TransactionStatus status = transactionManager.getTransaction(definition);
 
         try {
             String SQL1 = "insert into Student (name,age) values (?,?)";
@@ -33,14 +33,15 @@ public class StudentJDBCTemplate {
             String SQL3 = "insert into Marks(sid,marks,year) values(?,?,?)";
             jdbcTemplate.update(SQL3,sid,marks,year);
             System.out.println("Create Name = "+name+",Age = "+age);
-            transactionManager.commit(status);
+//            transactionManager.commit(status);
+//            throw new RuntimeException("simulate Error condition");
         } catch (DataAccessException e){
             System.out.println("Error in create record,rooling back");
-            transactionManager.rollback(status);
+//            transactionManager.rollback(status);
             throw e;
         }
 
-        return;
+//        return;
 
     }
 
@@ -70,7 +71,7 @@ public class StudentJDBCTemplate {
         return;
     }
 
-    public void setTransactionManager(PlatformTransactionManager transactionManager) {
-        this.transactionManager = transactionManager;
-    }
+//    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+//        this.transactionManager = transactionManager;
+//    }
 }
